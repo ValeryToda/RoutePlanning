61f551df69559920fdc0a74c06be22cf15cca065
Toda <toda.valerygmail.com>
2021-03-13 23:41:56 +0100

    Main parameter added

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,18 @@
 
 using namespace std::experimental;
 
+void ChooseValues(float &start_x, float &start_y, float &end_x, float &end_y)
+{
+    std::cout << "Please enter x value of start position: "; 
+    std::cin >> start_x;
+    std::cout << "Please enter y value of start position: "; 
+    std::cin >> start_y;
+    std::cout << "Please enter x value of end position: "; 
+    std::cin >> end_x;
+    std::cout << "Please enter y value of end position: "; 
+    std::cin >> end_y;
+}
+
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
     std::ifstream is{path, std::ios::binary | std::ios::ate};
@@ -52,15 +64,18 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+    // Declare floats `start_x`, `start_y`, `end_x`, and `end_y`
+    float start_x, start_y, end_x, end_y;
+    
+    // Get user input for these values using std::cin. 
+    ChooseValues(start_x, start_y, end_x, end_y);
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    // Pass the user input to the RoutePlanner object.
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
